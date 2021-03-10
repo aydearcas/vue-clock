7bde9cad6d2a3034418bc1affbcbb3c05403a8d5
Aythami de Armas <aydearcas@gmail.com>
2021-03-10 21:27:07 +0000

    Initial version of the vue-clock, functional. Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="dark-box">
+      <div class="hours">{{hours}}</div>
+      <div class="dots">:</div>
+      <div class="mins">{{mins}}</div>
+      <div class="dots">:</div>
+      <div class="secs">{{secs}}</div>
+      <div class=""></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      hours: 0,
+      mins: 0,
+      secs: 0
+    };
+  },
+  methods: {
+    setTime () {
+      setInterval(() => {
+        const date = new Date();
+        this.hours = date.getHours().toString().padStart(2, '0');
+        this.mins = date.getMinutes().toString().padStart(2, '0');
+        this.secs = date.getSeconds().toString().padStart(2, '0');
+      }, 1000);
+    }
+  },
+  mounted () {
+    this.setTime();
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=DotGothic16&display=swap');
+  strong {
+    color: aquamarine;
+  }
+  p {
+    color:greenyellow;
+  }
+
+  .container {
+    background: linear-gradient(to right, grey, rgb(46, 44, 44));
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 700px;
+    height: 200px;
+  }
+
+  .dark-box {
+    font-family: 'DotGothic16', sans-serif;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 600px;
+    height: 150px;
+    background: black;
+    border-radius: 20px;
+    color: yellow;
+    font-size: 120px;
+  }
 </style>
